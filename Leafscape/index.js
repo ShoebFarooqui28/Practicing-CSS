b3ef96b298b649f3b8ed0ab1d7548a6a1ee068d2
ShoebFarooqui28 <farooquishoeb2@gmail.com>
2025-03-09 22:12:00 +0530

    Done with the Home Page

--- Leafscape/index.js
+++ Leafscape/index.js
@@ -1,4 +1,15 @@
 function toggleMenu() {
-  let navLinks = document.querySelector(".nav__links");
-  navLinks.classList.toggle("show");
+  const navLinks = document.querySelector(".nav__links");
+  if (navLinks.style.display === "flex") {
+    navLinks.style.display = "none";
+  } else {
+    navLinks.style.display = "flex";
+    navLinks.style.flexDirection = "column";
+    navLinks.style.position = "absolute";
+    navLinks.style.top = "80px";
+    navLinks.style.right = "20px";
+    navLinks.style.backgroundColor = "#344e41";
+    navLinks.style.padding = "1rem";
+    navLinks.style.borderRadius = "5px";
+  }
 }
